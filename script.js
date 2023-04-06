406eebd0d60e9051441d94094bac59f3fc9ab6da
Jakub Janik <65724150+Hakciu@users.noreply.github.com>
2023-04-06 13:53:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -23,19 +23,22 @@ const golden = document.querySelector("#golden");
 const logo = document.querySelector("#logo");
 const gif = document.querySelector("#gif");
 
-let lives = 9999;
+// Ustawianie max życia na 99999 bo nie chciało mi się tego zmieniać w kodzie
+let lives = 99999;
 let maxHealth;
 
-if (lives >= 9999) {
+if (lives >= 99999) {
   live.checked = false;
   livesChecked.style.display = "none";
   showLives.style.display = "none";
 }
 
+// Sprawdzanie ustwień.
 let speedChecked = false;
 let liveChecked = false;
 let goldenChecked = false;
 
+// Włączanie złotych krecików
 golden.addEventListener("click", () => {
   if (golden.checked) {
     goldenChecked = true;
@@ -44,6 +47,7 @@ golden.addEventListener("click", () => {
   }
 });
 
+// Włączanie żyć i wybór ich wartości
 live.addEventListener("click", () => {
   if (live.checked) {
     showLives.style.display = "block";
@@ -68,12 +72,13 @@ live.addEventListener("click", () => {
   } else {
     live.checked = false;
     livesChecked.style.display = "none";
-    lives = 9999;
-    maxHealth = 9999;
+    lives = 99999;
+    maxHealth = 99999;
     showLives.style.display = "none";
   }
 });
 
+// Włączanie zwiększającej się prędkości
 speed.addEventListener("click", () => {
   if (speed.checked) {
     speedChecked = true;
@@ -82,9 +87,11 @@ speed.addEventListener("click", () => {
   }
 });
 
+// Zmienne do losowania czy w danej rundzie pojawi się bomba i złoty krecik
 let random1 = 0;
 let random2 = 0;
 
+// Obsługa rozpoczynania gry z animacją
 startButton.addEventListener("click", () => {
   startButton.classList.add("btnn");
 
@@ -114,6 +121,7 @@ startButton.addEventListener("click", () => {
   }, 2000);
 });
 
+// Obsługa przejście do opcji z animacją
 optionsButton.addEventListener("click", () => {
   optionsButton.classList.add("btnn");
 
@@ -138,13 +146,15 @@ optionsButton.addEventListener("click", () => {
   }, 200);
 });
 
+// Ustawienie startowych wartości
 let score = 0;
-
 let speed1 = 500;
 let speed2 = 1000;
 let speed3 = 1500;
 
+// Funkcja odpowiadająca za rozpoczącie gry
 function run() {
+  // Losowanie pojawienie się bomb i złotych krecików
   random1 = Math.floor(Math.random() * 2);
   random2 = Math.floor(Math.random() * 4);
 
@@ -171,10 +181,10 @@ function run() {
   img2.setAttribute("draggable", "false");
   img3.setAttribute("draggable", "false");
 
-
   img.classList.add("mole");
   img.src = "assets/mole.png";
 
+  // Zwykły krecik
   img.addEventListener("click", () => {
     score++;
     if (speedChecked === true) {
@@ -193,6 +203,7 @@ function run() {
     }, speed1);
   });
 
+  // Bomba
   if (random1 === 0) {
     img3.classList.add("mole");
     img3.src = "assets/bomba3.png";
@@ -215,6 +226,14 @@ function run() {
           resultResult.textContent = score;
         }, 500);
       }
+
+      if (liveChecked == false) {
+        score -= 5;
+        if (score <= 0) {
+          score = 0;
+        }
+        scoreElement.textContent = score;
+      }
       livesElement.textContent = lives;
       img3.src = "assets/bomba2.png";
       clearTimeout(timer3);
@@ -228,6 +247,7 @@ function run() {
     hole3.appendChild(img3);
   }
 
+  // Złoty krecik
   if (goldenChecked == true) {
     if (random2 === 0) {
       img2.classList.add("mole");
@@ -238,7 +258,7 @@ function run() {
       }, 150);
 
       img2.addEventListener("click", () => {
-        score += 2;
+        score += 5;
         scoreElement.textContent = score;
         img2.src = "assets/mole-whacked2.png";
         clearTimeout(timer2);
@@ -251,12 +271,19 @@ function run() {
 
   hole.appendChild(img);
 
+  // Usunięcie krecika po upływie czasu. Zmniejszenie punktów o 1
   timer = setTimeout(() => {
     hole.removeChild(img);
+    score--;
+    if (score < 0) {
+      score = 0;
+    }
+    scoreElement.textContent = score;
     run();
   }, speed3);
 }
 
+// Przyciski powrotu
 backButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
     btn.classList.add("btnn");
@@ -286,20 +313,23 @@ backButtons.forEach((btn) => {
   });
 });
 
-
+// obliczanie przesuniecie młotka
 window.addEventListener("mousemove", (e) => {
   cursor.style.top = e.pageY - 15 + "px";
   cursor.style.left = e.pageX + 15 + "px";
 });
 
+// Animacja uderzenia
 window.addEventListener("mousedown", () => {
   cursor.classList.add("active");
 });
 
+// Animacja powrotu młotka
 window.addEventListener("mouseup", () => {
   cursor.classList.remove("active");
 });
-//window onload
+
+// Animacja na ekranie główym. Logo i bomby
 window.addEventListener("load", () => {
   logo.classList.add("pulse");
 
